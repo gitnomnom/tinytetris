16dd7cc4176ccc32ba44af9875f24be3714c3347
gitnomnom <lotti@fs.lmu.de>
2019-02-12 15:56:08 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='tinytetris',
+      version='0.1',
+      description='Tetris implementation in Python',
+      url='https://github.com/gitnomnom/tinytetris',
+      author='CDM',
+      author_email='',
+      license='Apache License, Version 3.0',
+      packages=['tinytetris'],
+      zip_safe=False)
