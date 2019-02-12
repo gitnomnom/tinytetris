a885b91f828340309dd66b7e9ecc88b154f4c0ad
gitnomnom <lotti@fs.lmu.de>
2019-02-12 16:06:58 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ URL = 'https://github.com/gitnomnom/tinytetris'
 NAME = 'tinytetris'
 DESC = 'Tetris implementation in Python' 
 AUTHOR = 'CDM'
-EMAIL = NONE
+
 
 setup(name=NAME,
       version=None,
       description=DESC,
       url=URL,
       author=AUTHOR,
-      author_email=EMAIL,
+      author_email=None,
       license='Apache License, Version 3.0',
       packages=['tinytetris'],
       python_requires=REQUIRES_PYTHON)
